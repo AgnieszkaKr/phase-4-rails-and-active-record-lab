e0732795706ba6fbc2bcc937bb30374986324ebd
Agnes <agsawicka@gmail.com>
2022-08-30 17:59:28 -0400

    commit tree deaa1df1670577a786297b319ca1b60cf781a993

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Aga", last_name: "Krokosz", grade: "4")
+Student.create!(first_name: "Ewelina", last_name: "Sawicka", grade: "5")
+Student.create!(first_name: "Michal", last_name: "Krokosz", grade: "5")
+Student.create!(first_name: "Lukaszewicz", last_name: "Hanna", grade: "6")
+Student.create!(first_name: "Antoni", last_name: "Sawicki", grade: "4")
